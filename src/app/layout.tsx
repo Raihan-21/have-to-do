b3bc9c312f36627aad878198dbb372c27e4cbf7a
Muhammad Raihan <muhammadraihan118@gmail.com>
2024-12-31 19:35:16 +0700

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Have To Do</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
